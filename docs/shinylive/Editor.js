03250c626ed942c6abe77fdd0cb6eb96cacaccd2
alvarolf90 <alvaroledoferreira@gmail.com>
2026-09-22 13:07:33 -0300

    Atualiza shinylive para v0.5.0.9000 (assets 0.10.15) para tentar corrigir erro de Service Worker no Safari/iPhone

--- docs/shinylive/Editor.js
+++ docs/shinylive/Editor.js
@@ -1,4 +1,4 @@
-// Shinylive 0.10.12
+// Shinylive 0.10.15
 // Copyright 2026 Posit, PBC
 import {
   Icon,
@@ -13,7 +13,7 @@ import {
   require_jsx_runtime,
   require_react,
   stringToUint8Array
-} from "./chunk-JPKZOFRI.js";
+} from "./chunk-IVHHOOEQ.js";
 import {
   __commonJS,
   __esm,
@@ -23,7 +23,7 @@ import {
   __publicField,
   __toCommonJS,
   __toESM
-} from "./chunk-UV7YCL3S.js";
+} from "./chunk-LYVYCXL6.js";
 
 // node_modules/events/events.js
 var require_events = __commonJS({
